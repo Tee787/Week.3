08503ffb11a32d66b4f5134f7918d8aabf879ae9
Tee787 <tawhiri.tamihana@gmail.comgit config --global user.name Tawhirimatea>
2018-08-06 18:17:03 +1200

    first commit

---
+++ IfPractices/IfPractices/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfPractices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comparing Integers Exercise
+
+            /*int a, b;
+
+            Console.WriteLine("______________Welcome!________________");
+            Console.WriteLine("");
+            Console.WriteLine("Please enter your first number: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter your second number: ");
+            b = Int32.Parse(Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine("{0} is greater than {1}", a, b);
+                Console.Read();
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("a {0} is equal to b", a);
+                Console.Read();
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("a {0} is les than b", a);
+            }
+
+            Console.ReadLine();*/
+
+            //Guess my number Exercise
+
+            int myNum, userNum;
+
+            Console.WriteLine("______________Welcome!______________");
+            Console.WriteLine("");
+            Console.WriteLine("")
+
+
+        }
+    }
+}
